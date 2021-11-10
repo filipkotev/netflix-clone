d884046ef7b1621878e0aa63819144afef9f8364
Filip Kotev <filipkotev@yahoo.com>
2021-11-10 15:32:53 +0200

    Adding global-styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { GlobalStyles } from './global-styles';
 
-ReactDOM.render(
-      <App />,
+ReactDOM.render(<>
+  <GlobalStyles/>
+    <App />
+  </>,
   document.getElementById('root')
 );
