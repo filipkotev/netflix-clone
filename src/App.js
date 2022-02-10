bd9a177f0093a1125fedf1afc68a947291bed4a2
Filip Kotev <filipkotev@yahoo.com>
2022-02-10 19:03:05 +0200

    Setting up routes(react-router-dom), creating empty pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Signin, Signup, Browse } from './pages/pageIndex';
 
 function App() {
   return (
-    <>
-    <JumbotronContainer />
-    <FaqsContainer />
-    <FooterContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />}/>
+        <Route exact path={ROUTES.BROWSE} element={<Browse />} />
+        <Route exact path={ROUTES.SIGN_UP} element={<Signup />} />
+        <Route exact path={ROUTES.SIGN_IN} element={<Signin />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/pageIndex.js
@@ -0,0 +1,5 @@
+// Keep this index as reference to all pages we create
+export { default as Home } from './home';
+export { default as Browse } from './browse';
+export { default as Signin } from './signin';
+export { default as Signup } from './signup';
